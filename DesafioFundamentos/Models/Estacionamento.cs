80fd7afe5a307b401c29735c76099f2e1bad9dbc
Kauan <cesarkauanferreira@hotmail.com>
2024-05-06 21:40:18 -0300

    feat: update Estacionamento.cs fuctions add, read and remove cars

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -37,14 +37,18 @@ namespace DesafioFundamentos.Models
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
-                int horas = ;
+                int horas = 0;
                 decimal valorTotal = 0; 
 
+                Int32.TryParse(Console.ReadLine(), out horas);    
+
+                valorTotal = precoInicial + (precoPorHora * horas);
+                
 
 
                 // TODO: Remover a placa digitada da lista de veículos
                 // *IMPLEMENTE AQUI*
-
+                veiculos.Remove(placa);
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
             }
             else
@@ -61,6 +65,11 @@ namespace DesafioFundamentos.Models
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
                 // *IMPLEMENTE AQUI*
+
+                foreach (var Carro in veiculos)
+                {
+                    Console.WriteLine(Carro);
+                }
             }
             else
             {
